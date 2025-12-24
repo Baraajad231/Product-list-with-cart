ea8951fd428f8434ba806ea5a65777c2f61404c2
BARAA A. A. JADALLAH <brajadallh9@gmail.com>
2025-12-24 12:46:04 +0300

    Delete scripts directory

--- scripts/elements.js
+++
@@ -1,5 +0,0 @@
-export const productsDiv = document.querySelector(".products");
-export const cartList = document.querySelector(".cart__list");
-export const cartTotalValue = document.querySelector(".cart__total-value");
-export const cartEmpty = document.querySelector(".cart__empty");
-export const cartItems = document.querySelector(".cart__items");
